b0e72bca84c8a6285c264c31ffb1f0aedbfa8992
Marcelo Miqueles <marcelomiqueles@hotmail.com>
2015-01-20 12:21:40 -0300

    Diseño inicial
    Se aplicó el diseño inicial sin efectos ni animaciones.

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta content="width=device-width, initial-scale=1" name="viewport">
+		<title>Just a Drop</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+		<script type="text/javascript" src='js/script.js'></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id='bigBox'>
+			<div id='box-1'>
+				<div id='menu' class='bg-azul'>
+					<div id='menuBox'>
+						<a href="#">
+							<div id='logo'></div>
+						</a>
+						<div id='menuInteriorBox'>
+							<div id='separaPunto'>·</div>
+							<div id='textoMenu'>
+								<a href="#">
+									CÓMO FUNCIONA
+								</a>
+							</div>
+							<div id='separaPunto'>·</div>
+							<div id='textoMenu'>
+								<a href="#">
+									OSTOMÍA
+								</a>
+							</div>
+							<div id='separaPunto'>·</div>
+							<div id='textoMenu'>
+								<a href="#">
+									INDUSTRIAL
+								</a>
+							</div>
+							<div id='separaPunto'>·</div>
+							<div id='textoMenu'>
+								<a href="#">
+									CONTACTO
+								</a>
+							</div>
+							<div id='separaPunto'>·</div>
+							<div id='textoMenu'>
+								<a href="#">
+									PREGUNTAS FRECUENTES
+								</a>
+							</div>
+							<div id='separaPunto'>·</div>
+							<div id='textoMenu'>
+								<a href="#">
+									DÓNDE COMPRAR
+								</a>
+							</div>
+							<div id='separaPunto'>·</div>
+							<div id='redes' class='redesMenu'>
+								<a href='https://www.facebook.com/pages/Just-a-Drop-Chile/1399265286973574' target='_blank'>
+									<img class='fb-logo' src='img/fb-logo.png'>
+								</a>
+								<a href='https://www.twitter.com/JustaDropCL' target='_blank'>
+									<img class='tw-logo' src='img/tw-logo.png'>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id='imgHome'></div>
+			</div>
+			<div id='box-2'>
+				<div id='footBox'>
+					<div id='footBoxInterior'>
+						<div id='logoFooter'></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
